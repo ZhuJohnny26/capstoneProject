c9f4a990d2fa9aa8b923576532945901c0c196ab
msats5 <saterson@gmail.com>
2019-02-18 12:03:47 -0500

    added monk1 train and test

---
+++ R/data.R
@@ -0,0 +1,37 @@
+#' Monk's Problems Data Set (Problem 1 Training Set)
+#'
+#' The training set for problem 1 of the Monk's Problems Data Set,
+#' available from the UCI Machine Learning Repository.
+#'
+#' @source \url{https://archive.ics.uci.edu/ml/datasets/MONK's+Problems}
+#' @format A data frame with 124 rows and 8 columns:
+#' \describe{}
+#'   \item{label}{The class label}
+#'   \item{a1}{Attribute 1}
+#'   \item{a2}{Attribute 2}
+#'   \item{a3}{Attribute 3}
+#'   \item{a4}{Attribute 4}
+#'   \item{a5}{Attribute 5}
+#'   \item{a6}{Attribute 6}
+#'   \item{id}{A unique identification number}
+#' }
+"monks1_train"
+
+#' Monk's Problems Data Set (Problem 1 Test Set)
+#'
+#' The test set for problem 1 of the Monk's Problems Data Set,
+#' available from the UCI Machine Learning Repository.
+#'
+#' @source \url{https://archive.ics.uci.edu/ml/datasets/MONK's+Problems}
+#' @format A data frame with 432 rows and 8 columns:
+#' \describe{}
+#'   \item{label}{The class label}
+#'   \item{a1}{Attribute 1}
+#'   \item{a2}{Attribute 2}
+#'   \item{a3}{Attribute 3}
+#'   \item{a4}{Attribute 4}
+#'   \item{a5}{Attribute 5}
+#'   \item{a6}{Attribute 6}
+#'   \item{id}{A unique identification number}
+#' }
+"monks1_test"
