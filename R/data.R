d7ececad7f350adf13fd2f11ed8220dc131550d8
msats5 <saterson@gmail.com>
2019-02-22 14:43:11 -0500

    added documentation for monks1_test_full, monks1_test_partial

--- R/data.R
+++ R/data.R
@@ -34,4 +34,25 @@
 #'   \item{a6}{Attribute 6}
 #'   \item{id}{A unique identification number}
 #' }
-"monks1_test"
+"monks1_test_full"
+
+#' Monk's Problems Data Set (Problem 1 Test Set Subset)
+#'
+#' The test set for problem 1 of the Monk's Problems Data Set,
+#' excluding instances from the training set. Available from the
+#' UCI Machine Learning Repository.
+#'
+#' @source \url{https://archive.ics.uci.edu/ml/datasets/MONK's+Problems}
+#' @format A data frame with 308 rows and 8 columns:
+#' \describe{
+#'   \item{label}{The class label}
+#'   \item{a1}{Attribute 1}
+#'   \item{a2}{Attribute 2}
+#'   \item{a3}{Attribute 3}
+#'   \item{a4}{Attribute 4}
+#'   \item{a5}{Attribute 5}
+#'   \item{a6}{Attribute 6}
+#'   \item{id}{A unique identification number}
+#' }
+"monks1_test_partial"
+
