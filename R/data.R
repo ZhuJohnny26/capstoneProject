13d1b9f9e18345a354ebd9a45f273e2eeef32fbc
msats5 <saterson@gmail.com>
2019-03-01 01:16:52 -0500

    added data for all monks problems

--- R/data.R
+++ R/data.R
@@ -34,16 +34,15 @@
 #'   \item{a6}{Attribute 6}
 #'   \item{id}{A unique identification number}
 #' }
-"monks1_test_full"
+"monks1_test"
 
-#' Monk's Problems Data Set (Problem 1 Test Set Subset)
+#' Monk's Problems Data Set (Problem 2 Training Set)
 #'
-#' The test set for problem 1 of the Monk's Problems Data Set,
-#' excluding instances from the training set. Available from the
-#' UCI Machine Learning Repository.
+#' The training set for problem 2 of the Monk's Problems Data Set,
+#' available from the UCI Machine Learning Repository.
 #'
 #' @source \url{https://archive.ics.uci.edu/ml/datasets/MONK's+Problems}
-#' @format A data frame with 308 rows and 8 columns:
+#' @format A data frame with 169 rows and 8 columns:
 #' \describe{
 #'   \item{label}{The class label}
 #'   \item{a1}{Attribute 1}
@@ -54,5 +53,62 @@
 #'   \item{a6}{Attribute 6}
 #'   \item{id}{A unique identification number}
 #' }
-"monks1_test_partial"
+"monks2_train"
+
+#' Monk's Problems Data Set (Problem 2 Test Set)
+#'
+#' The test set for problem 2 of the Monk's Problems Data Set,
+#' available from the UCI Machine Learning Repository.
+#'
+#' @source \url{https://archive.ics.uci.edu/ml/datasets/MONK's+Problems}
+#' @format A data frame with 432 rows and 8 columns:
+#' \describe{
+#'   \item{label}{The class label}
+#'   \item{a1}{Attribute 1}
+#'   \item{a2}{Attribute 2}
+#'   \item{a3}{Attribute 3}
+#'   \item{a4}{Attribute 4}
+#'   \item{a5}{Attribute 5}
+#'   \item{a6}{Attribute 6}
+#'   \item{id}{A unique identification number}
+#' }
+"monks2_test"
+
+#' Monk's Problems Data Set (Problem 3 Training Set)
+#'
+#' The training set for problem 3 of the Monk's Problems Data Set,
+#' available from the UCI Machine Learning Repository.
+#'
+#' @source \url{https://archive.ics.uci.edu/ml/datasets/MONK's+Problems}
+#' @format A data frame with 122 rows and 8 columns:
+#' \describe{
+#'   \item{label}{The class label}
+#'   \item{a1}{Attribute 1}
+#'   \item{a2}{Attribute 2}
+#'   \item{a3}{Attribute 3}
+#'   \item{a4}{Attribute 4}
+#'   \item{a5}{Attribute 5}
+#'   \item{a6}{Attribute 6}
+#'   \item{id}{A unique identification number}
+#' }
+"monks3_train"
+
+#' Monk's Problems Data Set (Problem 3 Test Set)
+#'
+#' The test set for problem 3 of the Monk's Problems Data Set,
+#' available from the UCI Machine Learning Repository.
+#'
+#' @source \url{https://archive.ics.uci.edu/ml/datasets/MONK's+Problems}
+#' @format A data frame with 432 rows and 8 columns:
+#' \describe{
+#'   \item{label}{The class label}
+#'   \item{a1}{Attribute 1}
+#'   \item{a2}{Attribute 2}
+#'   \item{a3}{Attribute 3}
+#'   \item{a4}{Attribute 4}
+#'   \item{a5}{Attribute 5}
+#'   \item{a6}{Attribute 6}
+#'   \item{id}{A unique identification number}
+#' }
+"monks3_test"
 
