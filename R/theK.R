f27b446383e2542c1c1e373324621aa807ae6fb7
ZhuJohnny26 <44298471+ZhuJohnny26@users.noreply.github.com>
2019-03-12 12:53:24 -0400

    Add files via upload

--- R/theK.R
+++ R/theK.R
@@ -1,12 +1,6 @@
-#intialization
-#first you need to import data but setwd first 
-#bCancer <- read.table("/insertyourDatapath/wdbc.txt" , header=false, sep = ",")
-#delete first 2 columns
-#bCancer[1:2] <- NULL
-#create test set called testMean
-#testMean <- bCancer [1:569,1:5]
-#then source and run the script
-#k_pts has the table of mean points
+bCancer <- read.table("D:\\New folder\\wdbc.txt" , header=FALSE, sep = ",")
+bCancer[1:2] <- NULL
+testMean <- bCancer [1:569,1:5]
 
 myMeans <- colMeans(testMean)
 colMax <- function(data) sapply(data, max, na.rm = TRUE)
@@ -15,23 +9,34 @@ colMin <- function(data) sapply(data, min, na.rm = TRUE)
 myMax <-colMax(testMean)
 myMin <-colMin(testMean)
 
+k <- 3 #set k
+n <- 1000 #set number of iterations for recursion
+g <- 1 #convergence limit
+
 pointMean <- function(data, means, column) {
   mean_pts <- rep(0,3)
-  noMean <- means[column]
+  noMean <- means[column] #first mean
   subData <- data[1:569, column]
+  minData <- subData[subData < noMean] #data between min and first mean
+  maxData <- subData[subData > noMean] #data between first mean and max
+  mean_pts[1] <- noMean
   
-  minData <-subData[subData<noMean]
-  minMean <- mean(minData)
-  minData_1 <- minData[minData < minMean]
-  mean_pts[1] <- mean(minData_1)
-  
-  maxData <- subData[subData >noMean]
-  meanData_1 <- mean(maxData)
-  maxData_1 <- maxData[maxData< meanData_1]
-  mean_pts[2] <- mean(maxData_1)
-  maxData_2 <- maxData[maxData >meanData_1]
-  mean_pts[3] <- mean (maxData_2)
-
+  #at 1 k is placed in 2, 3
+  #at 2 k is placed in 4, 5 and so on
+  for(i in 1:floor(k/2)){ 
+    temp1 <- mean(minData)
+    temp2 <- mean(maxData)
+    mean_pts[i+i] <- temp1 #adds the means 
+    mean_pts[i+i+1]<- temp2
+    minData <- minData[minData < temp1]
+    maxData <- maxData[temp2 < maxData]
+    
+  }
+  if(k %% 2 == 0){  #if k is even gets rid of the first
+                    #mean to have even k means
+    mean_pts <- mean_pts[2:floor(k/2)] 
+ 
+  }
   return(mean_pts)
 }
 
@@ -47,7 +52,7 @@ k_pts <- data.frame(col1_mean, col2_mean, col3_mean, col4_mean, col5_mean)
 euclidean_dist <- function(k,unk) {
   distance <- 0
   distance <- sqrt((unk[1] - k[1])^2 + (unk[2] - k[2])^2 + (unk[3] - k[3])^2 + (unk[4] - k[4])^2
-                        + (unk[5] - k[5])^2)
+                   + (unk[5] - k[5])^2)
   
   return(distance)
 } 
@@ -63,7 +68,7 @@ for(i in 1:nrow(testMean)){
   mean_pt1 <- k_pts[1, 1:5]
   mean_pt2 <- k_pts[2, 1:5]
   mean_pt3 <- k_pts[3, 1:5]
-  get_dist[1]<- euclidean_dist(mean_pt1, need_dist)
+  get_dist[1] <- euclidean_dist(mean_pt1, need_dist)
   get_dist[2] <- euclidean_dist(mean_pt2, need_dist)
   get_dist[3] <- euclidean_dist(mean_pt3, need_dist)
   dist_1 <- as.numeric(as.character(get_dist[1]))
@@ -81,5 +86,6 @@ for(i in 1:nrow(testMean)){
     enter3 <- testMean[i, 1:5]
     clust_3 <- rbind(clust_3, enter3)
   }
- 
+  
 }
+
